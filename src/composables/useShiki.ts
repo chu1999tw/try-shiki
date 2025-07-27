f69178c50f0fa9acb31807eb31157cd8dec33f7b
aaronChang <chuwork.99@gmail.com>
2025-07-27 20:14:53 +0800

    feat: 簡化 sanitizeHtml 函數

--- src/composables/useShiki.ts
+++ src/composables/useShiki.ts
@@ -25,24 +25,13 @@ createHighlighterCore({
 })
 
 function sanitizeHtml(input: string): string {
-  // Create a sanitized version of the HTML input
-  // First remove potentially dangerous HTML elements
-  let output = input.replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '')
-  output = output.replace(/<(iframe|object|embed|form)[^>]*>[^<]*(?:<(?!\/(iframe|object|embed|form)>)[^<]*)*<\/\1>/gi, '')
-
-  // Remove dangerous attributes
-  output = output.replace(/\son\w+=".*?"/gis, '')
-  output = output.replace(/\son\w+='.*?'/gis, '')
-  output = output.replace(/href\s*=\s*["']?javascript:[^"']*["']/gi, 'href="#"')
-  output = output.replace(/src\s*=\s*["']?javascript:[^"']*["']/gi, '')
-  output = output.replace(/style\s*=\s*["'][^"']*expression\([^"']*["']/gi, '')
-
-  // Block sensitive words (add your list as needed)
-  const sensitiveWords = ['profanity', 'slur', 'offensive', 'inappropriate']
-  const sensitivePattern = new RegExp(sensitiveWords.join('|'), 'gi')
-  output = output.replace(sensitivePattern, '***')
-
-  return output
+  return input
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/`/g, '&#96;')
 }
 
 export function useHighlighter(code: string, lang: Lang = 'javascript') {
